3441fdf465a37e337a01ad961330f956ea0ba669
lehuy08012001 <lehuy0801ht@gmail.com>
2021-09-06 23:01:29 +0700

    message

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    BASE_URL: "http://localhost:3000",
+    MONGODB_URL:
+      "mongodb+srv://lehuy0801:huyhau0801@e-commerce.gayyy.mongodb.net/E-commerce?retryWrites=true&w=majority",
+    ACCESS_TOKEN_SECRET: "Thisisaccess_tokensecret",
+    REFRESH_TOKEN_SECRET: "Thisisrefreshtokensecret",
+  },
+  images: {
+    domains: ["cdn.icon-icons.com"],
+  },
+};
